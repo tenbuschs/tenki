9096bf619aa9ef160167037428eea6b1d8cc8dcc
tenbuschs <120258652+tenbuschs@users.noreply.github.com>
2023-03-31 19:30:06 +0200

    dynamic storage Lists from exampleData
    solved error in widget_test.dart and implemented the automatic List-Builder for different storage locations and their items

--- lib/gui_storage.dart
+++ lib/gui_storage.dart
@@ -1,87 +1,6 @@
 import 'package:flutter/material.dart';
 
 
-class VerticalTabBar extends StatefulWidget {
-  const VerticalTabBar({Key? key}) : super(key: key);
-
-  @override
-  _VerticalTabBarState createState() => _VerticalTabBarState();
-  }
-
-
-class _VerticalTabBarState extends State<VerticalTabBar> {
-
-  int selectedIndex = 0;
-  PageController _pageController = PageController();
-  int pagesCount=7;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Row(
-          children: [
-            SizedBox(
-              width: 100,
-              child: ListView.separated(
-                itemCount: pagesCount,
-                separatorBuilder: (BuildContext context, int index){
-                  return SizedBox(height: 5);
-                },
-                itemBuilder: (BuildContext context, int index){
-                  return GestureDetector(
-                    onTap: (){
-                      setState((){
-                      selectedIndex = index;
-                      _pageController.jumpToPage(index);
-                    });
-                    },
-                    child: Container(
-                    child: Row(
-                      children: [
-                        AnimatedContainer(
-                          duration: Duration(milliseconds: 400),
-                          height: (selectedIndex == index) ? 50 : 0,
-                          width: 5,
-                          color: Colors.teal,
-                        ),
-                        Expanded(
-                          child: AnimatedContainer(
-                            alignment: Alignment.center,
-                            duration: Duration(milliseconds: 500),
-                            height: 50,
-                            color: (selectedIndex == index) ? Colors.blueGrey.withOpacity(0.2) : Colors.transparent,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical:0, horizontal:5),
-                              child: Text('Lagerort $index'),
-                            ),
-                        ))
-                      ],
-                    ),
-                    ),
-                  );
-                },
-                ),
-               ),
-              Expanded(
-                child: Container(
-                  child: PageView(
-                    controller: _pageController,
-                    children: [
-                          Container(
-                            child:  storageTabContent(),
-                          )
-                    ],
-                  ),
-                )
-              )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 //Beispieldaten Lagerbestand
 Map<String, dynamic> storageMap = {
   "items": [
@@ -181,22 +100,142 @@ Map<String, dynamic> storageMap = {
       "stockQuantity": 2,
       "buyQuantity": 1,
     },
+    {
+      "name": "Tomaten, gehackt",
+      "location": "Vorratsschrank",
+      "unit": "Dosen",
+      "targetQuantity": 3,
+      "stockQuantity": 3,
+      "buyQuantity": 0,
+    },
+
+    {
+      "name": "Lösch mich, drück 'x'",
+      "location": "Kleiderschrank",
+      "unit": "Testeinheit",
+      "targetQuantity": 1,
+      "stockQuantity": 1,
+      "buyQuantity": 0,
+    },
   ],
 };
 
 
+class VerticalTabBar extends StatefulWidget {
+  const VerticalTabBar({Key? key}) : super(key: key);
+
+  @override
+  _VerticalTabBarState createState() => _VerticalTabBarState();
+  }
+
+
+class _VerticalTabBarState extends State<VerticalTabBar> {
+
+  int selectedIndex = 0;
+  PageController _pageController = PageController();
+
+  Set<String> locations = {};
+
+
+  void initState() {
+    super.initState();
+
+    // iterate over the list of items and collect the unique locations in a set
+    for (var item in storageMap["items"]) {
+      locations.add(item["location"]);
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            SizedBox(
+                width: 120,
+                child:  ListView.builder(
+                  itemCount: locations.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    String location = locations.elementAt(index);
+                    return GestureDetector(
+                      onTap: (){
+                        setState((){
+                          selectedIndex = index;
+                          _pageController.jumpToPage(index);
+                        });
+                      },
+                      child: Container(
+                        child: Row(
+                          children: [
+                            AnimatedContainer(
+                              duration: Duration(milliseconds: 400),
+                              height: (selectedIndex == index) ? 50 : 0,
+                              width: 5,
+                              color: Colors.teal,
+                            ),
+                            Expanded(
+                                child: AnimatedContainer(
+                                  alignment: Alignment.center,
+                                  duration: Duration(milliseconds: 500),
+                                  height: 50,
+                                  color: (selectedIndex == index) ? Colors.blueGrey.withOpacity(0.2) : Colors.transparent,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(vertical:0, horizontal:5),
+                                    child: Text(location),
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+            ),
+            Expanded(
+              child: Container(
+                child: PageView(
+                  controller: _pageController,
+                  children: [
+                        Container(
+                          child:  storageTabContent(location: locations.elementAt(selectedIndex)),
+                        )
+                  ],
+                ),
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
 
 // Baut den Inhalt der Lagerorte
-
 class storageTabContent extends StatefulWidget {
+  String location='as';
+
+  storageTabContent({this.location=''});
+
   @override
   _storageTabContentState createState() => _storageTabContentState();
 }
 
 class _storageTabContentState extends State<storageTabContent> {
-  final List<Map<String, dynamic>> _items = storageMap['items']
-      .where((item) => item['location'] == 'Keller')
-      .toList();
+  List<Map<String, dynamic>> _items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _items = storageMap['items']
+        .where((item) => item['location'] == widget.location)
+        .toList();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -211,7 +250,7 @@ class _storageTabContentState extends State<storageTabContent> {
             children: [
               Row(
                 children: [
-                  Text('Einheit: '),
+                  Text('      Einheit: '),
                   Expanded(
                     child: TextFormField(
                       initialValue: item['unit'].toString(),
@@ -226,7 +265,7 @@ class _storageTabContentState extends State<storageTabContent> {
               ),
               Row(
                 children: [
-                  Text('Bestand: '),
+                  Text('      Bestand: '),
                   Expanded(
                     child: TextFormField(
                       initialValue: item['stockQuantity'].toString(),
@@ -243,7 +282,7 @@ class _storageTabContentState extends State<storageTabContent> {
               ),
               Row(
                 children: [
-                  Text('Soll: '),
+                  Text('      Soll: '),
                   Expanded(
                     child: TextFormField(
                       initialValue: item['targetQuantity'].toString(),
