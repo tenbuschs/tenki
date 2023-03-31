807e2e13980d05b86c16f0a8b2e7bea2e13aea07
tenbuschs <120258652+tenbuschs@users.noreply.github.com>
2023-03-31 22:41:49 +0200

    implement delet item in gui (swiping)
    added gui to delet items with swiping left or right

--- lib/gui_storage.dart
+++ lib/gui_storage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 //Beispieldaten Lagerbestand
 Map<String, dynamic> storageMap = {
   "items": [
@@ -36,7 +35,7 @@ Map<String, dynamic> storageMap = {
       "stockQuantity": 0,
       "buyQuantity": 2,
     },
-   /* {
+    /* {
       "name": "Zwiebeln",
       "location": "Keller",
       "unit": "kg",
@@ -108,7 +107,6 @@ Map<String, dynamic> storageMap = {
       "stockQuantity": 3,
       "buyQuantity": 0,
     },
-
     {
       "name": "Lösch mich, drück 'x'",
       "location": "Kleiderschrank",
@@ -120,21 +118,17 @@ Map<String, dynamic> storageMap = {
   ],
 };
 
-
 class VerticalTabBar extends StatefulWidget {
   const VerticalTabBar({Key? key}) : super(key: key);
 
   @override
   _VerticalTabBarState createState() => _VerticalTabBarState();
-  }
-
+}
 
 class _VerticalTabBarState extends State<VerticalTabBar> {
-
   int selectedIndex = 0;
   PageController _pageController = PageController();
   Set<String> locations = {};
-
 
   void initState() {
     super.initState();
@@ -145,7 +139,6 @@ class _VerticalTabBarState extends State<VerticalTabBar> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -153,94 +146,92 @@ class _VerticalTabBarState extends State<VerticalTabBar> {
         child: Row(
           children: [
             SizedBox(
-                width: 120,
-                child:  Column(
-        children: [
-        Expanded(
-        child: ListView.builder(
-                  itemCount: locations.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    String location = locations.elementAt(index);
-                    return GestureDetector(
-                      onTap: (){
-                        setState((){
-                          selectedIndex = index;
-                          _pageController.jumpToPage(index);
-                        });
-                      },
-                      child: Container(
-                        child: Row(
-                          children: [
-                            AnimatedContainer(
-                              duration: Duration(milliseconds: 400),
-                              height: (selectedIndex == index) ? 50 : 0,
-                              width: 5,
-                              color: Colors.teal,
-                            ),
-                            Expanded(
-                                child: AnimatedContainer(
+              width: 120,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: locations.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        String location = locations.elementAt(index);
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              print('setState $index');
+                              selectedIndex = index;
+                              _pageController.jumpToPage(selectedIndex);
+                            });
+                          },
+                          child: Container(
+                            child: Row(
+                              children: [
+                                AnimatedContainer(
+                                  duration: Duration(milliseconds: 400),
+                                  height: (selectedIndex == index) ? 50 : 0,
+                                  width: 5,
+                                  color: Colors.teal,
+                                ),
+                                Expanded(
+                                    child: AnimatedContainer(
                                   alignment: Alignment.center,
                                   duration: Duration(milliseconds: 500),
                                   height: 50,
-                                  color: (selectedIndex == index) ? Colors.blueGrey.withOpacity(0.2) : Colors.transparent,
+                                  color: (selectedIndex == index)
+                                      ? Colors.blueGrey.withOpacity(0.2)
+                                      : Colors.transparent,
                                   child: Padding(
-                                    padding: const EdgeInsets.symmetric(vertical:0, horizontal:5),
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 0, horizontal: 5),
                                     child: Text(location),
                                   ),
                                 ))
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-
-        ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'New location',
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ],
-                ),
-
-
-
-
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'New location',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.add),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
             Expanded(
-              child: Container(
-                child: PageView(
-                  controller: _pageController,
-                  children: [
-                        Container(
-                          child:  storageTabContent(location: locations.elementAt(selectedIndex)),
-                        )
-                  ],
-                ),
-              )
-            )
+                child: Container(
+              child: PageView(
+                controller: _pageController,
+                children: [
+                  Container(
+                    child: storageTabContent(
+                        location: locations.elementAt(selectedIndex)),
+                  )
+                ],
+              ),
+            ))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-
         onPressed: () {
           showDialog(
             context: context,
@@ -248,7 +239,8 @@ class _VerticalTabBarState extends State<VerticalTabBar> {
               // define text editing controllers for the input fields
               TextEditingController nameController = TextEditingController();
               TextEditingController unitController = TextEditingController();
-              TextEditingController targetQuantityController = TextEditingController();
+              TextEditingController targetQuantityController =
+                  TextEditingController();
 
               // define the dialog content
               return AlertDialog(
@@ -286,16 +278,15 @@ class _VerticalTabBarState extends State<VerticalTabBar> {
                     },
                   ),
                   IconButton(
-                    icon: const Icon(
-                        Icons.check_circle,
-                        color: Colors.teal),
+                    icon: const Icon(Icons.check_circle, color: Colors.teal),
                     onPressed: () {
                       // retrieve the values entered by the user
                       String name = nameController.text;
                       String unit = unitController.text;
-                      int targetQuantity = int.tryParse(targetQuantityController.text) ?? 0;
+                      int targetQuantity =
+                          int.tryParse(targetQuantityController.text) ?? 0;
                       int stockQuantity = 0;
-                      int buyQuantity = targetQuantity-stockQuantity;
+                      int buyQuantity = targetQuantity - stockQuantity;
 
                       // Create a new map object with the new entry details
                       Map<String, dynamic> newEntry = {
@@ -309,13 +300,13 @@ class _VerticalTabBarState extends State<VerticalTabBar> {
 
                       // Add the new entry to the existing items list in the storageMap
                       List<Map<String, dynamic>> itemsList =
-                      List<Map<String, dynamic>>.from(storageMap["items"]);
+                          List<Map<String, dynamic>>.from(storageMap["items"]);
                       itemsList.addAll([newEntry]);
 
                       storageMap["items"] = itemsList;
 
                       // Refresh the TabView
-                     setState((){
+                      setState(() {
                         selectedIndex = selectedIndex;
                         _pageController.jumpToPage(selectedIndex);
                       });
@@ -329,7 +320,6 @@ class _VerticalTabBarState extends State<VerticalTabBar> {
             },
           );
         },
-
         child: const Icon(Icons.add),
         backgroundColor: Colors.teal,
       ),
@@ -337,13 +327,11 @@ class _VerticalTabBarState extends State<VerticalTabBar> {
   }
 }
 
-
-
 // Baut den Inhalt der Lagerorte
 class storageTabContent extends StatefulWidget {
-  String location='as';
+  String location = 'as';
 
-  storageTabContent({this.location=''});
+  storageTabContent({this.location = ''});
 
   @override
   _storageTabContentState createState() => _storageTabContentState();
@@ -360,73 +348,88 @@ class _storageTabContentState extends State<storageTabContent> {
         .toList();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: _items.length,
       itemBuilder: (BuildContext context, int index) {
         final item = _items[index];
-        return ListTile(
-          title: Text(item['name']),
-          subtitle: Column(
-          //  crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text('      Einheit: '),
-                  Expanded(
-                    child: TextFormField(
-                      initialValue: item['unit'].toString(),
-                      onFieldSubmitted: (value) => item['unit'] = value,
-                      decoration: InputDecoration(
-                        hintText: 'Einheit',
-                        border: InputBorder.none,
+        return Dismissible(
+          key: UniqueKey(),
+          background: Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 30, // Customize the height of the button.
+              width: 30, // Customize the width of the button.
+              child: Icon(Icons.delete, color: Colors.white),
+            ),
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.only(right: 10),
+          ),
+          onDismissed: (direction) {
+            // Implement the code to remove the item from the list here.
+          },
+          child: ListTile(
+            title: Text(item['name']),
+            subtitle: Column(
+              children: [
+                Row(
+                  children: [
+                    Text('      Einheit: '),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: item['unit'].toString(),
+                        onFieldSubmitted: (value) => item['unit'] = value,
+                        decoration: InputDecoration(
+                          hintText: 'Einheit',
+                          border: InputBorder.none,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Text('      Bestand: '),
-                  Expanded(
-                    child: TextFormField(
-                      initialValue: item['stockQuantity'].toString(),
-                      keyboardType: TextInputType.number,
-                      onFieldSubmitted: (value) =>
-                      item['stockQuantity'] = double.parse(value),
-                      decoration: InputDecoration(
-                        hintText: 'Bestand',
-                        border: InputBorder.none,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text('      Bestand: '),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: item['stockQuantity'].toString(),
+                        keyboardType: TextInputType.number,
+                        onFieldSubmitted: (value) =>
+                        item['stockQuantity'] = int.parse(value),
+                        decoration: InputDecoration(
+                          hintText: 'Bestand',
+                          border: InputBorder.none,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Text('      Soll: '),
-                  Expanded(
-                    child: TextFormField(
-                      initialValue: item['targetQuantity'].toString(),
-                      keyboardType: TextInputType.number,
-                      onFieldSubmitted: (value) =>
-                      item['targetQuantity'] = double.parse(value),
-                      decoration: InputDecoration(
-                        hintText: 'Soll',
-                        border: InputBorder.none,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text('      Soll: '),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: item['targetQuantity'].toString(),
+                        keyboardType: TextInputType.number,
+                        onFieldSubmitted: (value) =>
+                        item['targetQuantity'] = int.parse(value),
+                        decoration: InputDecoration(
+                          hintText: 'Soll',
+                          border: InputBorder.none,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-            ],
+                  ],
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         );
       },
     );
+
   }
 }
 
