397f9a2249664409d2a39cd4bac23ff249e345af
tenbuschs <120258652+tenbuschs@users.noreply.github.com>
2023-04-11 17:32:14 +0200

    Implement Pascals Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'gui_storage.dart' as storage_tab;
 import 'gui_shopping-list.dart' as shopping_list_tab;
@@ -45,6 +46,105 @@ class _TenkiHomePageState extends State<TenkiHomePage>
         bottom: TabBar(
           controller: _tabController,
           tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.storage),
+            ),
+            Tab(
+              icon: Icon(Icons.shopping_cart),
+            ),
+            Tab(
+              icon: Icon(Icons.cookie),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: [
+                        const storage_tab.VerticalTabBar(),
+                        const shopping_list_tab.ShoppingList(),
+                        recipe_tab.Recipe(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+import 'package:tenki/widget_tree.dart';
+import '../gui_storage.dart' as storage_tab;
+import '../gui_shopping-list.dart' as shopping_list_tab;
+import '../gui_recipe.dart' as recipe_tab;
+
+
+import 'package:firebase_core/firebase_core.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const TenkiApp());
+}
+class TenkiApp extends StatelessWidget {
+  const TenkiApp({Key?key}) :super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'TENKI App',
+        home: const WidgetTree()
+    );
+  }
+}
+
+class TenkiHomePage extends StatefulWidget {
+  @override
+  _TenkiHomePageState createState() => _TenkiHomePageState();
+}
+
+class _TenkiHomePageState extends State<TenkiHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TENKI - für deinen smarten Alltag'),
+        backgroundColor: Colors.teal,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+
             Tab(
               icon: Icon(Icons.storage),
             ),
---
+++ lib/widget_tree.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tenki/home_page.dart';
+import 'package:tenki/login_register_page.dart';
+import 'package:flutter/material.dart';
+import 'package:tenki/auth.dart';
+import 'package:tenki/main.dart';
+//import 'package:tenki/Pages/login_register_page.dart';
+
+class WidgetTree extends StatefulWidget{
+  const WidgetTree({Key? key}): super(key:key);
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else{
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
