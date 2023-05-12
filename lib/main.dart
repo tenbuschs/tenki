259f14b4e725bee84d9f9096cd96daeae0d521aa
PascalKlH <130033640+PascalKlH@users.noreply.github.com>
2023-05-12 08:16:12 +0200

    setup for web connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:TENKI/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,7 +6,22 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyD9pHD6hlGebyM5t29HzAyPqGjxDn0cmL0",
+        authDomain: "tenki-1d439.firebaseapp.com",
+        projectId: "tenki-1d439",
+        storageBucket: "tenki-1d439.appspot.com",
+        messagingSenderId: "675339380935",
+        appId: "1:675339380935:web:09b1fbaab331825d9476e2",
+      )
+    );
+  }else{
+    await Firebase.initializeApp();
+  }
+
+
   runApp(const TenkiApp());
 }
 
